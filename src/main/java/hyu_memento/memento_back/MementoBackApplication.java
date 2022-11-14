de4ff07c9cd4dfe2acbb9ce2e1ff78c8fe4a8426
hanuel <ezzeezz@naver.com>
2022-11-14 13:51:52 +0900

    initial commit

---
+++ src/main/java/hyu_memento/memento_back/MementoBackApplication.java
@@ -0,0 +1,13 @@
+package hyu_memento.memento_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MementoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hyu_memento/memento_back/MementoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package hyu_memento.memento_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
