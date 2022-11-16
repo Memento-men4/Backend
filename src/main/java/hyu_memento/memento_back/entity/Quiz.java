9d34c7ecac0480fe992878d0f97303673a34d258
hanuel <ezzeezz@naver.com>
2022-11-16 16:56:56 +0900

    feat(entity): Add Quiz entity

---
+++ src/main/java/hyu_memento/memento_back/entity/Quiz.java
@@ -0,0 +1,51 @@
+package hyu_memento.memento_back.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quiz_seq")
+    private Long seq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_seq")
+    private Member member;
+
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL)
+    private List<QuizContent> quizContents = new ArrayList<>();
+
+    private String datetime;
+    private int result;
+
+    @Builder
+    public Quiz(Member member, String datetime) {
+        this.member = member;
+        member.getQuizs().add(this);
+        this.datetime = datetime;
+    }
+
+    //==연관관계 메서드==//
+//    public void setMember(Member member) {
+//        this.member = member;
+//        member.getQuizs().add(this);
+//    }
+
+    public void addQuizContent(QuizContent quizContent) {
+        quizContents.add(quizContent);
+        quizContent.setQuiz(this);
+    }
+
+    // TODO : result 계산해서 넣어주는 method
+}
