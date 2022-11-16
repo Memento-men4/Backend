bd2814b6b7888cba0d564cdd1fbe82c997033711
hanuel <ezzeezz@naver.com>
2022-11-16 17:06:30 +0900

    feat(entity): Add GamePlay entity

---
+++ src/main/java/hyu_memento/memento_back/entity/GamePlay.java
@@ -0,0 +1,45 @@
+package hyu_memento.memento_back.entity;
+
+import hyu_memento.memento_back.type.GameType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
+public class GamePlay {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gameplay_seq")
+    private Long seq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_seq")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private GameType type;
+
+    private String datetime;
+
+    private int result;
+
+    @Builder
+    public GamePlay(Member member, GameType type, String datetime, int result) {
+        this.member = member;
+        member.getGamePlays().add(this);
+        this.type = type;
+        this.datetime = datetime;
+        this.result = result;
+    }
+
+//    //==연관관계 메서드==//
+//    public void setMember(Member member) {
+//        this.member = member;
+//        member.getGamePlays().add(this);
+//    }
+}
