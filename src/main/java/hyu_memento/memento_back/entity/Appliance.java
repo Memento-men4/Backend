3ab9d864b188cbe3b22b0eec6ce6db62d7266cf1
hanuel <ezzeezz@naver.com>
2022-11-16 17:07:20 +0900

    feat(entity): Add Appliance entity

---
+++ src/main/java/hyu_memento/memento_back/entity/Appliance.java
@@ -0,0 +1,51 @@
+package hyu_memento.memento_back.entity;
+
+import hyu_memento.memento_back.type.ApplianceStatus;
+import hyu_memento.memento_back.type.ApplianceType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
+public class Appliance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "appliance_seq")
+    private Long seq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_seq")
+    private Member member;
+
+    @OneToMany(mappedBy = "appliance", cascade = CascadeType.ALL)
+    private List<ApplianceOperation> applianceOperations = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private ApplianceType type;
+
+    @Enumerated(EnumType.STRING)
+    private ApplianceStatus status;
+
+    private String settingTime;
+
+    @Builder
+    public Appliance(Member member, ApplianceType type, ApplianceStatus status, String settingTime) {
+        this.member = member;
+        member.getAppliances().add(this);
+        this.type = type;
+        this.status = status;
+        this.settingTime = settingTime;
+    }
+
+    public void addApplianceOperation(ApplianceOperation applianceOperation) {
+        applianceOperations.add(applianceOperation);
+        applianceOperation.setAppliance(this);
+    }
+}
