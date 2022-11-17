72c1b5760456bfa5c726bd488ccabee65b911a38
hanuel <ezzeezz@naver.com>
2022-11-17 20:10:10 +0900

    feat(member): Add MemberRepository

---
+++ src/main/java/hyu_memento/memento_back/repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package hyu_memento.memento_back.repository;
+
+import hyu_memento.memento_back.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long seq) {
+        return em.find(Member.class, seq);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+}
