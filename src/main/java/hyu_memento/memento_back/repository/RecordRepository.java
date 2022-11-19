cffb5ade9ce47498112cadd6c3d7c8604abfde16
hanuel <ezzeezz@naver.com>
2022-11-19 17:12:27 +0900

    feat(record): Add RecordRepository

--- src/main/java/hyu_memento/memento_back/repository/RecordRepository.java
+++ src/main/java/hyu_memento/memento_back/repository/RecordRepository.java
@@ -1,2 +1,31 @@
-package hyu_memento.memento_back.repository;public class RecordRepository {
+package hyu_memento.memento_back.repository;
+
+import hyu_memento.memento_back.domain.Member;
+import hyu_memento.memento_back.domain.Record;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class RecordRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Record record) {
+        em.persist(record);
+    }
+
+    public Record findOne(Long seq) {
+        return em.find(Record.class, seq);
+    }
+
+    public List<Record> findByDate(LocalDate date) {
+        return em.createQuery("select r from Record r where r.date = :date", Record.class)
+                .setParameter("date", date)
+                .getResultList();
+    }
 }
