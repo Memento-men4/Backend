3c4a3a86ecec40503a1c6ae820d9bb35a8746530
hanuel <ezzeezz@naver.com>
2022-11-25 14:03:46 +0900

    refactor(game): controller단에는 dto만

--- src/main/java/hyu_memento/memento_back/controller/GameplayController.java
+++ src/main/java/hyu_memento/memento_back/controller/GameplayController.java
@@ -19,70 +19,29 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class GameplayController {
-    public final MemberService memberService;
     public final GameplayService gameplayService;
 
     /* 게임 기록 생성 */
     @PostMapping("/game")
     public Long save(@RequestBody GameSaveDto gameSaveDto) {
-        GamePlay game = GamePlay.builder()
-                .member(memberService.findOne(gameSaveDto.getMember_seq()))
-                .type(gameSaveDto.getType())
-                .date(gameSaveDto.getDate())
-                .time(gameSaveDto.getTime())
-                .result(gameSaveDto.getResult())
-                .build();
-        return gameplayService.saveGamePlay(game);
+        return gameplayService.save(gameSaveDto);
     }
 
     /* 게임기록 조회 */
     @GetMapping("/game")
     public List<GameReturnDto> allGame(@RequestParam Long member_seq) {
-        List<GamePlay> findGames = gameplayService.findAllGamePlay(member_seq);
-        List<GameReturnDto> returnGames = new ArrayList<>();
-        for (GamePlay game : findGames) {
-            GameReturnDto returnGame = GameReturnDto.builder()
-                    .type(game.getType())
-                    .date(game.getDate())
-                    .time(game.getTime())
-                    .result(game.getResult())
-                    .build();
-            returnGames.add(returnGame);
-        }
-        return returnGames;
+        return gameplayService.findAllGamePlay(member_seq);
     }
 
     /* 게임기록 종류별 조회 */
     @GetMapping("/game/type")
     public List<GameReturnDto> typeGame(@RequestParam GameType type,  @RequestParam Long member_seq) {
-        List<GamePlay> findGames = gameplayService.findTypeGamePlay(type, member_seq);
-        List<GameReturnDto> returnGames = new ArrayList<>();
-        for (GamePlay game : findGames) {
-            GameReturnDto returnGame = GameReturnDto.builder()
-                    .type(game.getType())
-                    .date(game.getDate())
-                    .time(game.getTime())
-                    .result(game.getResult())
-                    .build();
-            returnGames.add(returnGame);
-        }
-        return returnGames;
+        return gameplayService.findTypeGamePlay(type, member_seq);
     }
 
     /* 게임기록 날짜별 조회 */
     @GetMapping("/game/date")
     public List<GameReturnDto> dateGame(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date, @RequestParam Long member_seq) {
-        List<GamePlay> findGames = gameplayService.findDayGamePlay(date, member_seq);
-        List<GameReturnDto> returnGames = new ArrayList<>();
-        for (GamePlay game : findGames) {
-            GameReturnDto returnGame = GameReturnDto.builder()
-                    .type(game.getType())
-                    .date(game.getDate())
-                    .time(game.getTime())
-                    .result(game.getResult())
-                    .build();
-            returnGames.add(returnGame);
-        }
-        return returnGames;
+        return gameplayService.findDayGamePlay(date, member_seq);
     }
 }
--- src/main/java/hyu_memento/memento_back/service/GameplayService.java
+++ src/main/java/hyu_memento/memento_back/service/GameplayService.java
@@ -1,5 +1,7 @@
 package hyu_memento.memento_back.service;
 
+import hyu_memento.memento_back.controller.dto.GameReturnDto;
+import hyu_memento.memento_back.controller.dto.GameSaveDto;
 import hyu_memento.memento_back.domain.GamePlay;
 import hyu_memento.memento_back.domain.Member;
 import hyu_memento.memento_back.domain.type.GameType;
@@ -9,38 +11,79 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GameplayService {
-
+    public final MemberService memberService;
     private final GameplayRepository gameplayRepository;
 
-    /* 게임기록생성 */
+    /* 게임 기록 생성 */
     @Transactional
-    public Long saveGamePlay(GamePlay gamePlay) {
-        return gameplayRepository.save(gamePlay);
+    public Long save(GameSaveDto gameSaveDto) {
+        GamePlay game = GamePlay.builder()
+                .member(memberService.findOne(gameSaveDto.getMember_seq()))
+                .type(gameSaveDto.getType())
+                .date(gameSaveDto.getDate())
+                .time(gameSaveDto.getTime())
+                .result(gameSaveDto.getResult())
+                .build();
+        return gameplayRepository.save(game);
+    }
+
+    /* (개인별) 모든 게임 기록 조회 */
+    public List<GameReturnDto> findAllGamePlay(Long member_seq) {
+        List<GamePlay> findGames = gameplayRepository.findAll(member_seq);
+        List<GameReturnDto> returnGames = new ArrayList<>();
+        for (GamePlay game : findGames) {
+            GameReturnDto returnGame = GameReturnDto.builder()
+                    .type(game.getType())
+                    .date(game.getDate())
+                    .time(game.getTime())
+                    .result(game.getResult())
+                    .build();
+            returnGames.add(returnGame);
+        }
+        return returnGames;
+    }
+
+    /* (개인별) 종류별 게임 기록 조회 */
+    public List<GameReturnDto> findTypeGamePlay(GameType type, Long member_seq) {
+        List<GamePlay> findGames = gameplayRepository.findByType(type, member_seq);
+        List<GameReturnDto> returnGames = new ArrayList<>();
+        for (GamePlay game : findGames) {
+            GameReturnDto returnGame = GameReturnDto.builder()
+                    .type(game.getType())
+                    .date(game.getDate())
+                    .time(game.getTime())
+                    .result(game.getResult())
+                    .build();
+            returnGames.add(returnGame);
+        }
+        return returnGames;
+    }
+
+    /* (개인별) 날짜별 게임 기록 조회 */
+    public List<GameReturnDto> findDayGamePlay(LocalDate date, Long member_seq) {
+        List<GamePlay> findGames = gameplayRepository.findByDate(date, member_seq);
+        List<GameReturnDto> returnGames = new ArrayList<>();
+        for (GamePlay game : findGames) {
+            GameReturnDto returnGame = GameReturnDto.builder()
+                    .type(game.getType())
+                    .date(game.getDate())
+                    .time(game.getTime())
+                    .result(game.getResult())
+                    .build();
+            returnGames.add(returnGame);
+        }
+        return returnGames;
     }
 
     /* seq(PK)로 조회 */
     public GamePlay findOne(Long seq) {
         return gameplayRepository.findOne(seq);
-    }
-
-    /* (개인별) 모든 게임 기록 조회 */
-    public List<GamePlay> findAllGamePlay(Long member_seq) {
-        return gameplayRepository.findAll(member_seq);
-    }
-
-    /* (개인별) 날짜별 게임 기록 조회 */
-    public List<GamePlay> findDayGamePlay(LocalDate date, Long member_seq) {
-        return gameplayRepository.findByDate(date, member_seq);
-    }
-
-    /* (개인별) 종류별 게임 기록 조회 */
-    public List<GamePlay> findTypeGamePlay(GameType type,Long member_seq) {
-        return gameplayRepository.findByType(type, member_seq);
     }
 }
